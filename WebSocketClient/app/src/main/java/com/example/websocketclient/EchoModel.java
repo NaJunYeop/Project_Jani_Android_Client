4373520a30f28209e08d8dceeef4893d73959a20
NaJunYeop <skwnsduq2@naver.com>
2019-11-18 19:16:54 +0900

    tset01

---
+++ WebSocketClient/app/src/main/java/com/example/websocketclient/EchoModel.java
@@ -0,0 +1,17 @@
+package com.example.websocketclient;
+
+public class EchoModel {
+    private String echo;
+
+    public EchoModel() {
+
+    }
+
+    public String getEcho() {
+        return echo;
+    }
+
+    public void setEcho(String echo) {
+        this.echo = echo;
+    }
+}
