0ba560ede36263c669d2c4d4a980f1a3d6ad4d9a
NaJunYeop <nnjy1992@naver.com>
2019-12-31 17:01:06 +0900

    Temp_Merge

--- WebSocketClient/app/src/main/java/com/example/websocketclient/models/MessageModel.java
+++ WebSocketClient/app/src/main/java/com/example/websocketclient/models/MessageModel.java
@@ -1,36 +1,5 @@
 package com.example.websocketclient.models;
 
-<<<<<<< HEAD
-public class MessageModel {
-    private String userName;
-    private String phoneNumber;
-    private String email;
-
-    public static class Builder implements Buildable<MessageModel> {
-        private String userName;
-        private String phoneNumber;
-        private String email = "absent";
-
-        public Builder(String userName, String phoneNumber) {
-            this.userName = userName;
-            this.phoneNumber = phoneNumber;
-        }
-
-        public Builder setEmail(String email) {
-            this.email = email;
-            return this;
-        }
-
-        @Override
-        public MessageModel build() {
-            return new MessageModel(this);
-        }
-    }
-    public MessageModel(Builder builder) {
-        this.userName = builder.userName;
-        this.phoneNumber = builder.phoneNumber;
-        this.email = builder.email;
-=======
 import java.util.StringTokenizer;
 
 public class MessageModel {
@@ -97,6 +66,5 @@ public class MessageModel {
 
     public void setContents(String contents) {
         this.contents = contents;
->>>>>>> develop
     }
 }
